934aa7870ee3d9a6a91b0ebb7da0043f55d2eecc
Niraj-Echocode <156087349+Niraj-Echocode@users.noreply.github.com>
2024-03-23 01:54:59 +0530

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const index = () => {
+  return (
+    <View className="h-full flex justify-center items-center bg-gray-400">
+      <Text>index</Text>
+    </View>
+  );
+};
+
+export default index;
